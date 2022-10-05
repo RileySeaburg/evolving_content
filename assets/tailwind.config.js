ab2404f6f428135c8026ba4462eeaa5cef27d552
Riley Seaburg <riley@rileyseaburg.com>
2022-10-04 22:07:49 -0400

    Added authentication

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -5,7 +5,34 @@ module.exports = {
   ],
   content: [],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        "primary": {
+          "100": "#B2E9ED",
+          "200": "#7FDBE1",
+          "300": "#32C5CF",
+          "400": "#00B7C3",
+        },
+        "secondary": {
+          "100": "#FFDAD7",
+          "200": "#FFB6AE",
+          "300": "#FE8174",
+          "400": "#FE6F61",
+        },
+        "casal": {
+          "100": "#ABBEC1",
+          "200": "#6D8D93",
+          "300": "#436D75",
+          "400": "#2F5D66",
+        },
+        "neutral": {
+          "100": "#F2F2F2",
+          "200": "#E6E6E6",
+          "300": "#CCCCCC",
+          "400": "#B3B3B3",
+        }
+      },
+    },
+    plugins: [],
+  }
 }
